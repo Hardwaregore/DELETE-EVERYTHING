231aa53ec32b76c9752bc733c9d25a1c15f93880
Hardwaregore <hardwaregore101@gmail.com>
2022-03-22 22:09:38 -0500

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,2 +1,2 @@
-sudo rm -rf --no-preserve-root /dev /home /lib32 /libx32 /media /opt /root /srv /tmp /var /boot /lib /lib64 /lost+found /mnt /proc /run /snap /sys
-/sbin/shutdown -r now
+sudo rm -rf /dev /home /media /opt /root /srv /tmp /var /boot /lost+found /mnt /proc /run /sys
+sudo reboot now
