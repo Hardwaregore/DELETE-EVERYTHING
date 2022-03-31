884704e9b3cfffed03c583339a48ac28735e334e
Hardwaregore <hardwaregore101@gmail.com>
2022-03-31 12:07:59 -0500

    Update options.sh

--- run/options.sh
+++ run/options.sh
@@ -10,11 +10,11 @@ echo "X: Abort and self destruct"
 while true; do
     read -p "Please choose an option $ " yesno
     case $yesno in
-    [Aa]* ) source options.sh && run/ClearPersonalFiles.sh;;
-    [Bb]* ) exit && source run/clear.sh;;
-    [Cc]* ) exit && source run/destroy.sh;;
-    [Dd]* ) exit && source run/nuke.sh;;
-    [Xx]* ) exit && source run/selfdestruct.sh;;
+    [Aa]* ) source options.sh && ClearPersonalFiles.sh;;
+    [Bb]* ) exit && source clear.sh;;
+    [Cc]* ) exit && source destroy.sh;;
+    [Dd]* ) exit && source nuke.sh;;
+    [Xx]* ) exit && source selfdestruct.sh;;
     * ) echo "[A/B/C/D/X]";;
     
     esac
