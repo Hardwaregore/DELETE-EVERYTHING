ce84cbb4f9ca361645b2de1605f20271204c57ec
Hardwaregore <hardwaregore101@gmail.com>
2022-03-31 11:33:21 -0500

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -2,7 +2,7 @@
 echo "DELETE EVERYTHING"
 echo ".................."
 while true; do
-    read -p "Do you wish to continue? ANSWERING NO WILL ABORT AND SELF DESTRUCT" yesno
+    read -p "Do you wish to continue? ANSWERING NO WILL ABORT AND SELF DESTRUCT $ " yesno
     case $yesno in
     [Yy]* ) source options.sh && exit;;
     [Nn]* ) exit && rm -rf *;;
