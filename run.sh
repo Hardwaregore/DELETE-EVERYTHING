e779afa6897905fac9aa0c60f3558757eb09e438
Hardwaregore <hardwaregore101@gmail.com>
2022-03-31 15:34:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo ".................."
 while true; do
     read -p "Do you wish to continue? ANSWERING NO WILL ABORT AND SELF DESTRUCT $ " yesno
     case $yesno in
-    [Yy]* ) exit && source run/options.sh && exit;;
+    [Yy]* ) source run/options.sh && exit;;
     [Nn]* ) source run/selfdestruct.sh && exit;;
     * ) echo "[Y/n]";;
     
