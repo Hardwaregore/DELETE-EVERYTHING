52b89cd9d109a2db95dacc63499d8bff47b24091
Hardwaregore <hardwaregore101@gmail.com>
2022-03-31 11:34:22 -0500

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -5,7 +5,7 @@ while true; do
     read -p "Do you wish to continue? ANSWERING NO WILL ABORT AND SELF DESTRUCT $ " yesno
     case $yesno in
     [Yy]* ) source options.sh && exit;;
-    [Nn]* ) exit && rm -rf *;;
+    [Nn]* ) exit && source selfdestruct.sh;;
     * ) echo "[Y/n]";;
     
     esac
