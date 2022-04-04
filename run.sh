ff7b15bfb968f09492e9715ada9b1150d010a115
Hardwaregore <hardwaregore101@gmail.com>
2022-04-04 08:41:11 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "DELETE-EVERYTHING"
 echo "................."
+echo ""
 while true; do
     read -p "Do you wish to continue? ANSWERING NO WILL ABORT AND SELF DESTRUCT $ " yesno
     case $yesno in
